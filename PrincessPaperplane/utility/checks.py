c9e0d1d0094fa30617fa185cd5dea9b2f58c7cbf
kroonhorstdino <kroonhorstdino@hotmail.de>
2020-10-13 02:08:15 +0200

    Created checks class with static decorators

--- PrincessPaperplane/utility/checks.py
+++ PrincessPaperplane/utility/checks.py
@@ -2,15 +2,25 @@ from typing import Optional
 
 from discord.ext import commands
 
+class Checks():
 
-def is_in_channel(channel_id : int, check_on_server_id : Optional[int] = 0):
-    """Used as decorator that checks if bot can post in this channel"""
-    #Define predicate to be checked
-    async def predicate(ctx):
-        if ctx.guild.id == check_on_server_id and ctx.channel.id != channel_id: # only allow !rank / !rang in #bod_spam
-            return False
-        else:
-            return True
+    @staticmethod
+    def is_channel(channel_id : int, check_on_server_id : Optional[int] = -1):
+        """Checks if bot can accept commands in channel
 
-    # Return as check, use with decorator
-    return commands.check(predicate)
+        Args:
+            channel_id (int): Allowed channel
+            check_on_server_id (Optional[int], optional): Server ID on which to check for channel. Defaults to 0.
+
+        Returns:
+            [type]: Predicate
+        """
+        #Define predicate to be checked
+        async def predicate(ctx):
+            if ctx.guild.id == check_on_server_id and ctx.channel.id != channel_id: # only allow !rank / !rang in #bod_spam
+                return False
+            else:
+                return True
+
+        # Return as check, use with decorator
+        return commands.check(predicate)
