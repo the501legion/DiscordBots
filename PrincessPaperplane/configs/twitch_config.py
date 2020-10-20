bb80e6a4790f7ac7af9110ef07d9caf7d929bc6a
Ruben Maurer <rubenmaurer@live.de>
2020-10-20 06:04:11 +0200

    Fixed twitch config

--- PrincessPaperplane/configs/twitch_config.py
+++ PrincessPaperplane/configs/twitch_config.py
@@ -2,8 +2,8 @@ import twitch
 
 from cmds.Quotes import twitch_command_mapping
 
-twitch_chat = twitch.Chat(channel="#schrottler", nickname="Horst", oauth="oauth:d6xtl0ycqlx62io7qfr4o8mvf4dube",
-                          helix=twitch.Helix(client_id="krc3jjqwjuktzcx8xfavgr2v8o2owi", use_cache=True))
+twitch_chat = twitch.Chat(channel="#<CHANNEL>", nickname="PaperBot", oauth="oauth:<OAUTH>",
+                          helix=twitch.Helix(client_id="<CLIENT-ID>", use_cache=True))
 
 
 Mappings = {
