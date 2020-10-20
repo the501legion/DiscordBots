ffc5f4c0eb83d16e7bc41c86d5a5346d15a509e3
Ruben Maurer <rubenmaurer@live.de>
2020-10-20 16:14:55 +0200

    Add dotenv

--- PrincessPaperplane/paperbot.py
+++ PrincessPaperplane/paperbot.py
@@ -2,23 +2,26 @@
 # coding=utf-8
 
 import re  # Regex
-
 # import modules
-import configs.secret as secret
+from pathlib import Path
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+import configs.cmd_config as cmd_config
 import configs.guild_config as guild_config
 import configs.roles_config as roles_config
-import configs.cmd_config as cmd_config
-import discord
-import MySQLdb
-from discord.ext import commands
-
-import utility.db
+import configs.secret as secret
 from cmds.dice import Dice
 from cmds.quotes import Quotes
 from cmds.rank import Rank
 from cmds.roles import Roles
 from utility.cogs_enum import Cogs
 from utility.db import DB
+
+# load environment variables
+load_dotenv(dotenv_path=Path('.') / '.env')
 
 # create bot
 bot = commands.Bot(command_prefix=cmd_config.PREFIXES)
@@ -35,14 +38,15 @@ def add_cogs():
 
 add_cogs()
 
-DB : DB = bot.get_cog(Cogs.DB.value)
-ROLES : Roles = bot.get_cog(Cogs.ROLES.value)
+DB: DB = bot.get_cog(Cogs.DB.value)
+ROLES: Roles = bot.get_cog(Cogs.ROLES.value)
 
 # Discord API key
 API_KEY = secret.API_KEY
 
 prefixes_regex = '(' + "|".join(bot.command_prefix) + ')'
 DICE_CMD_REGEX = re.compile(r"^({prefix})([w,d])".format(prefix=prefixes_regex))
+
 
 # start bot
 @bot.event
@@ -62,24 +66,29 @@ async def on_ready():
         if guild.id == guild_config.TEST_SERVER:
             bot.user.name = bot.user.name + " (Test)"
 
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(name='twitch.tv/princesspaperplane', type=discord.ActivityType.watching))
+    await bot.change_presence(status=discord.Status.online,
+                              activity=discord.Activity(name='twitch.tv/princesspaperplane',
+                                                        type=discord.ActivityType.watching))
     print('------')
 
     await ROLES.update_reaction_msg(guild_config.ROLE_CHANNEL, roles_config.EMOTE_ROLES)
     await ROLES.update_reaction_msg(guild_config.ROLE_CHANNEL_TEST, roles_config.TEST_EMOTE_ROLES)
 
+
 @bot.event
 async def on_message(message):
     await handle_command(message)
 
+
 async def handle_command(message):
-        # Handle command stuff
+    # Handle command stuff
     match = DICE_CMD_REGEX.match(message.content)
     if bool(match):
-        #Split message content: !w6x8 becomes !w 6x8. Important for command extension, so it can extract parameters
+        # Split message content: !w6x8 becomes !w 6x8. Important for command extension, so it can extract parameters
         cmd_length = len(match.group(1)) + len(match.group(2))
         message.content = message.content[:cmd_length] + " " + message.content[cmd_length:]
     await bot.process_commands(message)
+
 
 # RUN BOT
 bot.run(API_KEY)
