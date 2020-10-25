9210632efdd936cb30962efb61cb11c6a0c3e4d6
501.legion <the501legion@users.noreply.github.com>
2020-10-25 02:39:01 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-tmux new-session -d -s paperbot-test 'python3 PrincessPaperplane/paperbot.py -test'
+tmux new-session -A -s paperbot-test 'python3 PrincessPaperplane/paperbot.py -test'
 tmux detach -s paperbot-test
 
