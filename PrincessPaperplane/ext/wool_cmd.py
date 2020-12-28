e2da4f1511ca41bac5a1a5beaf626d12a94ded0e
Ruben Maurer <rubenmaurer@live.de>
2020-12-28 17:17:04 +0100

    Add multiple confs for the wool command

--- PrincessPaperplane/ext/wool_cmd.py
+++ PrincessPaperplane/ext/wool_cmd.py
@@ -1,17 +1,25 @@
+from typing import List
+
 from discord.ext import commands
 from discord.ext.commands import Bot, Context
-from typing import List
 
 # configs
 ALIASES: List[str] = ['wool', 'wolle', 'wa']
-USER: int = 168781680864133120
 MESSAGE: str = "***Achtung!** {MENTION} will wieder Wolle kaufen!*"
+
+USER: int = 168781680864133120
+USER_STAGE: int = 762982903789125682
+USER_LOCAL: int = 255003995871903745
 
 
 # command group
 @commands.command(name="WoolAlert", aliases=ALIASES)
 async def wool_cmd(ctx: Context):
-    await ctx.channel.send(MESSAGE.format(MENTION=ctx.bot.get_user(USER).mention))
+    user = ctx.bot.get_user(USER)
+    user = ctx.bot.get_user(USER_STAGE) if user is None else user
+    user = ctx.bot.get_user(USER_LOCAL) if user is None else user
+
+    await ctx.channel.send(MESSAGE.format(MENTION=user.mention))
 
 
 def setup(bot: Bot):
