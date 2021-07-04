eff0a40ffcfe2932a4939d16fef301ce7aeb7efe
Nicola <nicola@nph-studios.de>
2021-07-04 14:20:19 +0000

    added limited wipe

--- PrincessPaperplane/cmds/wipe.py
+++ PrincessPaperplane/cmds/wipe.py
@@ -8,11 +8,18 @@ from configs.cmd_config import STRINGS, ALIASES
 class Wipe(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-    async def purge(self, channel):
+        
+    async def purge(self, channel, msgs=0):
+        if msgs == 0:
             await channel.purge()
-            await channel.send("Beachten Sie mich nicht, ich putze hier nur.")
-    @commands.command(aliases=ALIASES.WIPE.value)
-    async def cmd_wipe(self, ctx: commands.Context):
+        else:
+            await channel.purge(limit=msgs)
 
-        await self.purge(ctx.channel)
+    @commands.command(aliases=ALIASES.WIPE.value)
+    async def cmd_wipe(self, ctx: commands.Context, args):
+        try:
+            msgs = int(args) + 1
+        except:
+            msgs = 0
+        await self.purge(ctx.channel, msgs)
     
