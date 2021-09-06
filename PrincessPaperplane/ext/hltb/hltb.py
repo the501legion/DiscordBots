09dc2cff454c81afd659431fb0def0b8bad13e96
Ruben Maurer <rubenmaurer@live.de>
2021-09-06 17:03:48 +0200

    Fixed typo and added aliases

--- PrincessPaperplane/ext/hltb/hltb.py
+++ PrincessPaperplane/ext/hltb/hltb.py
@@ -8,21 +8,21 @@ class HLTB(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command('hltb')
+    @commands.command(name='hltb', aliases=['hl', 'howlong', 'howlongtobeat'])
     async def hltb(self, ctx: commands.Context, *, game: str):
         result_list = await HowLongToBeat().async_search(game)
         if result_list is not None and len(result_list) > 0:
             g: HowLongToBeatEntry = max(result_list, key=lambda element: element.similarity)
 
-            embeded = Embed(title=g.game_name)
-            embeded.set_author(name="How Long To Beat", url="https://howlongtobeat.com/")
-            embeded.add_field(name="Main Story", value=f"{g.gameplay_main} {g.gameplay_main_unit}")
-            embeded.add_field(name="Story + Extras", value=f"{g.gameplay_main_extra} {g.gameplay_main_extra_unit}")
-            embeded.add_field(name="Completionist", value=f"{g.gameplay_completionist} {g.gameplay_completionist_unit}")
+            embed = Embed(title=g.game_name)
+            embed.set_author(name="How Long To Beat", url="https://howlongtobeat.com/")
+            embed.add_field(name="Main Story", value=f"{g.gameplay_main} {g.gameplay_main_unit}")
+            embed.add_field(name="Story + Extras", value=f"{g.gameplay_main_extra} {g.gameplay_main_extra_unit}")
+            embed.add_field(name="Completionist", value=f"{g.gameplay_completionist} {g.gameplay_completionist_unit}")
 
-            embeded.add_field(name="Link", value=g.game_web_link)
-            embeded.set_thumbnail(url=f"https://howlongtobeat.com{g.game_image_url}")
+            embed.add_field(name="Link", value=g.game_web_link)
+            embed.set_thumbnail(url=f"https://howlongtobeat.com{g.game_image_url}")
 
-            return await ctx.channel.send(embed=embeded)
+            return await ctx.channel.send(embed=embed)
 
         return await ctx.channel.send(f"No results for {game} :(")
